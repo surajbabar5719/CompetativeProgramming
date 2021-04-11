e749ea22bb9b7e717fe5f6e8245a3380b3d1b6b6
Suraj Babar <43114638+surajbabar5719@users.noreply.github.com>
2021-04-11 15:49:17 +0530

    Create Picking Numbers.py

---
+++ HackerRank/Python/Practice/Algorithms/Implementation/Picking Numbers.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pickingNumbers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def pickingNumbers(a):
+    # Write your code here
+    set_a = set(a)
+    array=[]
+    for val in set_a:
+        array.append(a.count(val) + a.count(val+1))
+    return max(array)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = pickingNumbers(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
