df2e9cd749ad3b86909c0916decf946a20f0e7d3
Suraj Babar <43114638+surajbabar5719@users.noreply.github.com>
2021-04-14 22:05:44 +0530

    Create Viral Advertising.py

---
+++ HackerRank/Python/Practice/Algorithms/Implementation/Viral Advertising.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the viralAdvertising function below.
+def viralAdvertising(n):
+    shared = 5
+    liked = cum = 2
+    for i in range(n-1):
+        shared = liked * 3
+        liked = shared // 2
+        cum += liked
+    return cum
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    result = viralAdvertising(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
