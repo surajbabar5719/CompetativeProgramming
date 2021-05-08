08a861c20f706e64e2c0f18de29cc5fd10f4caf1
Suraj Babar <43114638+surajbabar5719@users.noreply.github.com>
2021-05-08 16:19:09 +0530

    Create Extra_Long_Factorials.py

---
+++ HackerRank/Python/Practice/Algorithms/Implementation/Extra_Long_Factorials.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'extraLongFactorials' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def extraLongFactorials(n):
+    # Write your code here
+    fact = 1
+    while n != 0:
+        fact *= n
+        n-=1
+    print(fact)
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    extraLongFactorials(n)
