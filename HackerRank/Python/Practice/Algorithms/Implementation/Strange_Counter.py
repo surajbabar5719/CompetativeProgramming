accb1ca56b873dc7ee0db9f7a32bd13c7a87532e
Suraj Babar <43114638+surajbabar5719@users.noreply.github.com>
2021-05-09 18:24:37 +0530

    Create Strange_Counter.py

---
+++ HackerRank/Python/Practice/Algorithms/Implementation/Strange_Counter.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'strangeCounter' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER t as parameter.
+#
+
+def strangeCounter(t):
+    # Write your code here
+    n=0
+    while n*2 + 3 < t:
+        n = n*2 +3
+    return  (n*2) + 4 - t
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    result = strangeCounter(t)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
